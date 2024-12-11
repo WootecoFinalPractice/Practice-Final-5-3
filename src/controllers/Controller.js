19b7f4b7a858d159221af74c0dfd430db21ab642
Beomtae <95863810+Beomtae@users.noreply.github.com>
2024-12-11 16:41:48 +0900

    ✨ feat: user 입력받기

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -5,7 +5,10 @@ class Controller {
   static async run() {
     const bridgeSize = await InputView.readBridgeSize();
     const bridge = Bridge.makeBridge(bridgeSize);
+    const userMove = await InputView.readMove();
+    Bridge.judge(bridge, userMove);
     console.log(bridge);
+    console.log(userMove);
   }
 }
 
--- src/views/InputView.js
+++ src/views/InputView.js
@@ -4,6 +4,10 @@ class InputView {
   static readBridgeSize() {
     return Console.readLineAsync(`다리 건너기 게임을 시작합니다.\n\n다리의 길이를 입력해주세요.\n`);
   }
+
+  static readMove() {
+    return Console.readLineAsync(`이동할 칸을 선택해주세요. (위: U, 아래: D)\n`);
+  }
 }
 
 export default InputView;
